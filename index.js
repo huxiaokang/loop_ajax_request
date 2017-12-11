4983cea4e8d46dcf94df997bb588ed7388056722
hukang <xuanlingzi214@sina.com>
2017-12-11 17:39:31 +0800

    index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+
+var currentTime = 1;
+var allLength = $("#historyInfo tbody").find(".refresh").length;
+dealRefreshAll(currentTime, allLength, _this, $("#historyInfo tbody"));
+
+function dealRefreshAll(currentTime,allLength,searchObj,dataObj){
+	if(currentTime > allLength){
+		searchObj.attr('data-operator',"true");
+		return;
+	}
+	var index = currentTime-1;
+	var _this = dataObj.find(".refresh").eq(index);
+	var goodsId = _this.parent("td").attr("data-attr");
+	var data = {goodsId:goodsId};
+	$.ajax({
+	    type:"GET",
+	    url:url,
+    	data:data,
+    	async:true,
+    	dateType:"json",
+    	success:function(data){
+    			var optInfo = data.data;
+    			if(data.code == 0){
+	    			var flag = optInfo.isOneRefresh;
+					if(flag){
+						initData();
+					}else{
+						var tdHtml = loadTdHtml(optInfo);
+			            $("#historyInfo tbody").find("tr").eq(index).html(tdHtml);
+					}
+    		
+    		currentTime++;
+    		dealRefreshAll(currentTime, allLength, searchObj, dataObj);
+    	},
+    	error:function(data){
+    		currentTime++;
+    		dealRefreshAll(currentTime, allLength, searchObj, dataObj);
+    	}
+    });
+}
